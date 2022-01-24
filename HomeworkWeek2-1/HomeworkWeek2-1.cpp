f33a1dc357ddb3b5181ebba36e2c78fcda650268
Joseph Cuesta Acevedo <N1001723@my.ntu.ac.uk>
2022-01-24 21:33:01 +0000

    Finished homework 1

--- HomeworkWeek2-1/HomeworkWeek2-1.cpp
+++ HomeworkWeek2-1/HomeworkWeek2-1.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
 using namespace std;
+#include <iostream>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 int main()
 {
-    cout << "Hello World!\n";
+	for (size_t i = 10; i > 0; i--)
+	{
+		cout << i << endl;
+		Sleep(1000);
+	}
+	cout << "Thunderbirds are go!" << endl;
 }
 
 
